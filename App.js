9fa690338b6b3382ef2f615ac9338e323378f300
Shanaka 'SHa' Senevirathne <shanaka36@gmail.com>
2018-12-10 21:49:23 +1100

    Setting up basic button

--- App.js
+++ App.js
@@ -1,13 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import Button from './src/Button'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,14 +9,15 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.instructions}>{"This is a normal button"}</Text>
+        <Button label="Primary button"/>
+        <View style={{height: 50}}/>
+        <Text style={styles.instructions}>{"This is a secondary button"}</Text>
+        <Button label="Secondary button" primary={false}/>
       </View>
     );
   }
@@ -34,12 +28,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    backgroundColor: '#fff',
   },
   instructions: {
     textAlign: 'center',
