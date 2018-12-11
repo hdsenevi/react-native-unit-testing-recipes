ee65e7ecb299ab25694b6c0bfb3ba26b3e29644a
Shanaka 'SHa' Senevirathne <shanaka36@gmail.com>
2018-12-12 00:32:34 +1100

    jest.mock()

--- src/Button.test.js
+++ src/Button.test.js
@@ -2,6 +2,13 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import Button from './Button';
 
+let mockOpenURL = jest.fn();
+
+// 1. Set openURL module function to jest.fn
+jest.mock('Linking', () => ({
+    openURL: mockOpenURL,
+}));
+
 describe('Button', () => {
     describe('Rendering', () => {
         it('should match to snapshot - Primary', () => {
@@ -16,21 +23,38 @@ describe('Button', () => {
 
     describe('Interaction', () => {
         describe('onPressHandler', () => {
+            const mockOnPress = jest.fn();
+            let instance;
+
+            // 2. preparing a reusable shallow rendered instance
+            beforeEach(() => {
+                instance = shallow(<Button 
+                    label="test label" 
+                    onPress={mockOnPress}
+                    url="https://www.test.com"
+                />).instance();
+                jest.clearAllMocks();
+            });
+
             it('should call onPress', () => {
-                // Arrange
-                const mockOnPress = jest.fn();      // 1. mock function
-                const component = shallow(<Button 
-                    label= "test label" 
-                    onPress={mockOnPress}           // 2. passing in mock function as props
-                />);
-                const instance = component.instance();  // 3. getting an instance of component
-
-                // Act
-                instance.onPressHandler();          // 4. manually triggering onPressHandler()
-
-                // Assert
+                instance.onPressHandler();
                 expect(mockOnPress).toHaveBeenCalled();
-                expect(mockOnPress).toHaveBeenCalledTimes(1);   // 5. checking return values
+                expect(mockOnPress).toHaveBeenCalledTimes(1);
+            });
+            
+            it('should call openURL if url is provided', () => {
+                instance.onPressHandler();
+                // 3. mockOpenURL should be called since we have passed in 'url' prop
+                expect(mockOpenURL).toHaveBeenCalled();
+                expect(mockOpenURL).toHaveBeenCalledTimes(1);
+                expect(mockOpenURL).toHaveBeenCalledWith("https://www.test.com");
+            });
+
+            it('should not call openURL if url is nor provided', () => {
+                const innerInstance = shallow(<Button label="test label" onPress={mockOnPress} />).instance();
+                innerInstance.onPressHandler();
+                // 4. mockOpenURL should NOT be called as we have NOT passed in 'url' prop
+                expect(mockOpenURL).not.toHaveBeenCalled();
             });
         });
     });
